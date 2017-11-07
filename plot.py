949b1fb1b9cb964017441744b63b0afd386e714f
Laura Koponen <laura.koponen@gmail.com>
2017-11-07 18:50:10 +0000

    Clean up plotting code

--- plot.py
+++ plot.py
@@ -1,11 +1,22 @@
+from collections import namedtuple
+
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 import pandas as pd
 import seaborn as sns
 
 
+Data = namedtuple("Data", ["filename", "title"])
+
 
 if __name__ == "__main__":
     sns.set()
+
+    data = [
+        Data("simple.csv", "Sequential training"),
+        Data("mixed.csv", "Training with the full dataset"),
+        Data("ewc.csv", "With EWC")
+    ]
 
     simple_df = pd.read_csv('simple.csv')
     group1 = simple_df[simple_df['Group'] == 1]
@@ -21,26 +32,27 @@ if __name__ == "__main__":
 
     fig = plt.figure()
 
-    ax1 = fig.add_subplot(311)
-    ax1.plot(group1['Epoch'], group1['TestAccuracy'])
-    ax1.plot(group2['Epoch'], group2['TestAccuracy'])
-    ax1.set_title('Sequential training')
-    ax1.set_xlabel('Epoch')
-    ax1.set_ylabel('Accuracy')
+    total_plots = len(data)
+    axes = []
+    dfs = []
 
-    ax2 = fig.add_subplot(312)
-    ax2.plot(mixed1['Epoch'], mixed1['TestAccuracy'])
-    ax2.plot(mixed2['Epoch'], mixed2['TestAccuracy'])
-    ax2.set_title('Training with the full dataset')
-    ax2.set_xlabel('Epoch')
-    ax2.set_ylabel('Accuracy')
+    for i in range(len(data)):
+        axes.append(fig.add_subplot(total_plots, 1, i+1))
+        ax = axes[-1]
 
-    ax3 = fig.add_subplot(313)
-    ax3.plot(ewc1['Epoch'], ewc1['TestAccuracy'])
-    ax3.plot(ewc2['Epoch'], ewc2['TestAccuracy'])
-    ax3.set_title('With EWC')
-    ax3.set_xlabel('Epoch')
-    ax3.set_ylabel('Accuracy')
+        dfs.append(pd.read_csv(data[i].filename))
+        df = dfs[-1]
+        group1 = df[df['Group'] == 1]
+        group2 = df[df['Group'] == 2]
+
+        ax.plot(group1['Epoch'], group1['TestAccuracy'])
+        ax.plot(group2['Epoch'], group2['TestAccuracy'])
+
+        ax.set_title(data[i].title)
+        ax.set_xlabel('Batch')
+        ax.set_ylabel('Accuracy')
+
+        ax.yaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:.0%}'.format(y)))
 
     plt.tight_layout()
 
